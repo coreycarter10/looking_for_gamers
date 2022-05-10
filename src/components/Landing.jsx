0cac5aae1afe83e5ec94e0d5910878efa5a5b056
Corey Carter <corey-carter@hotmail.com>
2022-05-09 18:50:34 -0700

    Started component work

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Login from "./Login";
+import SignUp from "./SignUp";
 
 const Landing = () => {
+  // return (<div>{!user ? <SignUp /> : <Login />}</div>);
   return (
     <div>
-      <h1>Looking For Group</h1>
+      <h1>Looking for Group</h1>
     </div>
   );
 };
